6aa541dfa9de7293a91d9d649991cfa5b6154afc
lefoy <me@lefoy.net>
2015-01-27 14:25:39 -0500

    Split js helpers into multiple files

---
+++ src/js/helpers/equalheight.js
@@ -0,0 +1,31 @@
+var $ = require('jquery');
+var _ = require('lodash');
+var waitForImages = require('waitforimages');
+
+module.exports = (function() {
+    var identifiers = [];
+
+    $(window).on('resize', _.debounce(function() {
+        $('[data-equalheight]').each(function() {
+            var id = $(this).data('equalheight');
+            if (!_.contains(identifiers, id)) identifiers.push(id);
+        });
+
+        identifiers.forEach(function(id) {
+            var height = 0,
+                elements = $('[data-equalheight=' + id + ']');
+
+            elements.waitForImages(function() {
+                elements.each(function() {
+                    var $this = $(this),
+                        currentHeight = 0;
+
+                    $this.css('height', 'auto');
+                    currentHeight = $this.outerHeight();
+                    height = (currentHeight > height ? currentHeight : height);
+                });
+                elements.outerHeight(height);
+            });
+        });
+    }, 100)).trigger('resize');
+}());
