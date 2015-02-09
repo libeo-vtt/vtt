be3f88bf57527f7365e8920489e54942b953c394
lefoy <me@lefoy.net>
2015-02-09 14:49:24 -0500

    Add vtt-templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 var $ = require('jquery');
+var upload = require('upload');
 var waitForImages = require('waitforimages');
 
 // Helpers
 var helpers = require('./helpers');
+
+// Make every upload input accessible
+$('input[type="file"]').accessibleUpload();
 
 // Webfont
 var Webfont = require('./modules/webfont.js');
