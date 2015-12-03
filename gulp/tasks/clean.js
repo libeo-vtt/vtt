f1147dbda9b0008bfb0eed5dd17c1f8fd1d0e606
Paul Côté <paul.cote@libeo.com>
2015-12-03 10:04:01 -0500

    Fixed clean task when using custom dest path

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,30 +1,31 @@
 var gulp = require('gulp');
 var del = require('del');
+var config = require('../config.js');
 
 gulp.task('clean', function(callback) {
-    return del(['build'], callback);
+    return del([config.build], callback);
 });
 
 gulp.task('clean:js', function(callback) {
-    return del(['build/js'], callback);
+    return del([config.build+'js'], callback);
 });
 
 gulp.task('clean:img', function(callback) {
-    return del(['build/img'], callback);
+    return del([config.build+'img'], callback);
 });
 
 gulp.task('clean:sass', function(callback) {
-    return del(['build/css'], callback);
+    return del([config.build+'css'], callback);
 });
 
 gulp.task('clean:twig', function(callback) {
-    return del(['build/*.html'], callback);
+    return del([config.build+'*.html'], callback);
 });
 
 gulp.task('clean:svg', function(callback) {
-    return del(['build/svg'], callback);
+    return del([config.build+'svg'], callback);
 });
 
 gulp.task('clean:twig', function(callback) {
-    return del(['build/*.html'], callback);
+    return del([config.build+'*.html'], callback);
 });
