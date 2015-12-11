a3c92de8b6f8b09213fb9830df71cd49492499c3
madam <mykael.adam@libeo.com>
2015-12-11 15:24:47 -0500

    Replaced default watch with gulp-watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
 var config = require('../config.js');
+var watch = require('gulp-watch');
 
 gulp.task('watch:js', ['browserify', 'copy:js']);
 gulp.task('watch:img', ['copy:img']);
@@ -10,19 +11,19 @@ gulp.task('watch:twig', ['twig']);
 gulp.task('watch:svg', ['svg2png', 'svgSprite', 'copy:svg']);
 
 gulp.task('watch', ['browser-sync'], function() {
-    gulp.watch(config.src + 'js/**/*', function() {
+    watch(config.src + 'js/**/*', function() {
         runSequence('clean:js', 'watch:js', browserSync.reload);
     });
-    gulp.watch(config.src + 'img/**/*', function() {
+    watch(config.src + 'img/**/*', function() {
         runSequence('clean:img', 'watch:img', browserSync.reload);
     });
-    gulp.watch(config.src + 'sass/**/*', function() {
+    watch(config.src + 'sass/**/*', function() {
         runSequence('clean:sass', 'watch:sass', browserSync.reload);
     });
-    gulp.watch(config.src + 'twig/**/*', function() {
+    watch(config.src + 'twig/**/*', function() {
         runSequence('clean:twig', 'watch:twig', browserSync.reload);
     });
-    gulp.watch(config.src + 'svg/**/*', function() {
+    watch(config.src + 'svg/**/*', function() {
         runSequence('clean:svg', 'watch:svg', browserSync.reload);
     });
 });
