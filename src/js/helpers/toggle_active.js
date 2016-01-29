f061be38f9cbb42c60e7ee36c87500be4a4d95e6
lefoy <lefoy@users.noreply.github.com>
2016-01-29 09:12:18 -0500

    Fix toggle_active helper global javascript name

--- src/js/helpers/toggle_active.js
+++ src/js/helpers/toggle_active.js
@@ -4,7 +4,7 @@ module.exports = (function() {
 
     var $body = $('body'),
         $document = $(document),
-        classes = window.TFO.Settings.Classes;
+        classes = window.PROJECT_NAME.Settings.Classes;
 
     $document.on('click', '[data-toggle-active]', function() {
         $('.' + $(this).attr('data-toggle-active')).toggleClass(classes.active);
