931e5fea6b9784a08f356878e790da69607348b5
lefoy <lefoy@users.noreply.github.com>
2016-02-05 12:47:54 -0500

    Fix IE issue with mq_breakpoints

--- src/js/helpers/mq_breakpoints.js
+++ src/js/helpers/mq_breakpoints.js
@@ -1,9 +1,18 @@
 var $ = require('jquery');
 
+function unquote(str) {
+    // Fix issue in Internet Explorer with JSON.parse and quotes
+    if (!$('html').hasClass('ie')) {
+        return false;
+    } else {
+        return str.slice(1, str.length - 1);
+    }
+}
+
 module.exports = (function() {
 
     $(window).on('resize', function() {
-        window.breakpoints = JSON.parse(JSON.parse(window.getComputedStyle(document.querySelector('body'), ':before').content));
+        window.breakpoints = JSON.parse(unquote(window.getComputedStyle(document.querySelector('body'), ':before').content));
     }).trigger('resize');
 
 }());
