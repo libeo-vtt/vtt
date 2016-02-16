faf2c324e7f735782b453db76b2ee89cdf431ae3
lefoy <lefoy@users.noreply.github.com>
2016-02-16 13:24:23 -0500

    Add multiple classes support for toggle_active helper

--- src/js/helpers/toggle_active.js
+++ src/js/helpers/toggle_active.js
@@ -3,11 +3,19 @@ var $ = require('jquery');
 module.exports = (function() {
 
     var $body = $('body'),
-        $document = $(document),
-        classes = window.PROJECT_NAME.Settings.Classes;
+        $document = $(document);
 
-    $document.on('click', '[data-toggle-active]', function() {
-        $('.' + $(this).attr('data-toggle-active')).toggleClass(classes.active);
+    $document.on('click', '[data-toggle-active]', function(event) {
+        var classes = $(this).attr('data-toggle-active');
+        if (classes.indexOf(' ') > 0) {
+            var classesArray = classes.split(' ');
+            for (var i = 0; i < classesArray.length; i++) {
+                $('.' + classesArray[i]).toggleClass(window.TFO.Settings.Classes.active);
+            }
+        } else {
+            $('.' + classes).toggleClass(window.TFO.Settings.Classes.active);
+        }
+        event.preventDefault();
     });
 
 }());
