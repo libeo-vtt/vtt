6f743bd3e28114a5b3a51d5ad672c6142c285878
lefoy <me@lefoy.net>
2016-04-05 13:55:56 -0400

    Disable default templates widgets

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,6 @@
 // This file is called after every other javascript files
 
 // Modules
-$('.tab').tab();
-$('form').form();
-$('.slider').slider();
-$('.folder-group').folder();
 
 // Google Fonts
 WebFont.load({
