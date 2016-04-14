00a4fd16104e1a6c064e33874cfc66bc6358740f
lefoy <me@lefoy.net>
2016-04-14 09:58:46 -0400

    Update customGlobalClasses default config

--- .templates/module.js
+++ .templates/module.js
@@ -16,7 +16,7 @@
             clicked: 'is-clicked',
             extern: 'is-external',
             error: 'is-error'
-        }, this.config.customGlobalClasses || {});
+        }, (window.classes !== undefined ? window.classes : this.config.customGlobalClasses) || {});
 
         this.init();
     };
