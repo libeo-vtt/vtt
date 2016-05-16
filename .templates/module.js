12299ad0599c0a85ef648c6f9bdda3654e805e69
lefoy <me@lefoy.net>
2016-05-16 14:20:25 -0400

    Update javascript module template

--- .templates/module.js
+++ .templates/module.js
@@ -7,6 +7,10 @@
 
         // Default module configuration
         this.defaults = {
+            labels: {
+                label1: 'label1',
+                label2: 'label2'
+            },
             classes: {
                 class1: 'class1',
                 class2: 'class2',
@@ -17,10 +21,19 @@
         };
 
         // Merge default classes with window.project.classes
-        this.classes = $.extend(true, this.defaults.classes, window.project.classes || {});
+        this.classes = $.extend(true, this.defaults.classes, (window.project ? window.project.classes : {}));
+
+        // Merge default labels with window.project.labels
+        this.labels = $.extend(true, this.defaults.labels, (window.project ? window.project.labels : {}));
 
         // Merge default config with custom config
         this.config = $.extend(true, this.defaults, options || {});
+
+        this.publicMethods = {
+            methodName: $.proxy(function() {
+                console.log('methodName function called');
+            }, this)
+        }
 
         this.init();
     };
@@ -35,17 +48,24 @@
     });
 
     $.fn.MODULE_NAME_LOWERCASE = function(options) {
-        return this.each(function() {
-            var element = $(this);
+        this.each($.proxy(function(index, element) {
+            var $element = $(element);
 
-            // Return early if this element already has a plugin instance
-            if (element.data('MODULE_NAME_LOWERCASE')) return;
+            // Return early if this $element already has a plugin instance
+            if ($element.data('MODULE_NAME_LOWERCASE')) return;
 
-            // pass options to plugin constructor
-            var MODULE_NAME_LOWERCASE = new MODULE_NAME_UPPERCASE(this, options);
+            // Pass options to plugin constructor
+            var MODULE_NAME_LOWERCASE = new MODULE_NAME_UPPERCASE(element, options);
 
-            // Store plugin object in this element's data
-            element.data('MODULE_NAME_LOWERCASE', MODULE_NAME_LOWERCASE);
-        });
+            // Add every public methods to plugin
+            for (var key in MODULE_NAME_LOWERCASE.publicMethods) {
+                this[key] = MODULE_NAME_LOWERCASE.publicMethods[key];
+            }
+
+            // Store plugin object in this $element's data
+            $element.data('MODULE_NAME_LOWERCASE', MODULE_NAME_LOWERCASE);
+        }, this));
+
+        return this;
     };
 })(jQuery);
